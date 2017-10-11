63171b56e5dc50f53d86734683f66d8233cb58d2
Alex S <ac.shultz587@gmail.com>
2017-10-11 18:59:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,8 +36,10 @@ function viewCart() {
 
   //CORRECT UNTIL THIS POINT
   } else {                                                                      //2 or more items in the cart...
-
+    //[bananas 17, apples 5]
+    //[index0elem1, index1,elem2], length is 2
     for (let i = 0; i < cart.length; i++){                                      //iterate thru the cart
+      //assuming these statements are correct
       itemName = Object.keys(cart[i])[0]                                        //get the item name
       itemPrice = cart[i][itemName]                                             //get the item price
 
